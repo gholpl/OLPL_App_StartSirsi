ba84a5c987e7f47c7cbd426c577e32384545204b
gholpl <license@olpl.org>
2015-06-29 09:39:16 -0500

    Initial Commit

---
+++ OLPL_App_StartSirsi/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OLPL_App_StartSirsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            functions fn1 = new functions();
+            Settings st1 = new Settings();
+            st1 = Settings_Functions.getSettings();
+            Environment.SetEnvironmentVariable("CLASSPATH", st1.var_ENV_User, EnvironmentVariableTarget.User);
+            if(File.Exists(st1.location_Workflows + "\\new_custom.jar"))
+            {
+                try
+                {
+                    if (File.Exists(st1.location_Workflows + "\\custom.jar"))
+                    {
+                        File.Delete(st1.location_Workflows + "\\custom.jar");
+                    }
+                    Console.WriteLine("Overriding custom.jar with new file");
+                    File.Move(st1.location_Workflows + "\\new_custom.jar", st1.location_Workflows + "\\custom.jar");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else { Console.WriteLine("No new Custom.jar detected"); }
+            if (!File.Exists(st1.location_Workflows + "\\Updates\\upd_jwf.exe"))
+            {
+                Console.WriteLine("No new Updates detected");
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Starting Sirsi client with no Arguments");
+                    try
+                    {
+                        Process pr = new Process();
+                        ProcessStartInfo prs = new ProcessStartInfo();
+                        prs.UseShellExecute = true;
+                        prs.FileName = st1.location_Workflows + "\\Jre\\bin\\javaw.exe";
+                        prs.WorkingDirectory = st1.location_Workflows;
+                        prs.Arguments = "-Dworkflows.directory="+ "\"" + st1.location_Workflows + "\"" + " -Xms512m -Xmx512m -jar workflows.jar";
+                        pr.StartInfo = prs;
+                        pr.Start();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Starting Sirsi client with " + args[0] + " Argument");
+                    try
+                    {
+                        Process pr = new Process();
+                        ProcessStartInfo prs = new ProcessStartInfo();
+                        prs.UseShellExecute = true;
+                        prs.FileName = st1.location_Workflows + "\\Jre\\bin\\javaw.exe";
+                        prs.WorkingDirectory = st1.location_Workflows;
+                        prs.Arguments = "-Dworkflows.directory=" + "\"" + st1.location_Workflows + "\"" + " -Xms512m -Xmx512m -jar workflows.jar -w" + args[0];
+                        pr.StartInfo = prs;
+                        pr.Start();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Updating Sirsi Client");
+                try
+                {
+                    if (File.Exists(st1.location_Workflows + "\\Updates\\new_jwf.exe"))
+                    {
+                        File.Delete(st1.location_Workflows + "\\Updates\\new_jwf.exe");
+                    }
+                    Console.WriteLine("Overriding upd_jwf.jar with new file");
+                    File.Move(st1.location_Workflows + "\\Updates\\upd_jwf.exe", st1.location_Workflows + "\\Updates\\new_jwf.exe");
+                    Process pr = new Process();
+                    ProcessStartInfo prs = new ProcessStartInfo();
+                    prs.UseShellExecute = true;
+                    prs.FileName = st1.location_Workflows + "\\Updates\\new_jwf.exe";
+                    prs.WorkingDirectory = st1.location_Workflows;
+                    prs.Arguments = "";
+                    pr.StartInfo = prs;
+                    pr.Start();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+          
+
+
+           Thread.Sleep(6000);
+
+        }
+    }
+}
---
+++ OLPL_App_StartSirsi/Settings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLPL_App_StartSirsi
+{
+    public class Settings
+    {
+        public string location_Install { get; set; }
+        public string location_Workflows { get; set; }
+        public string var_ENV_User { get; set; }
+
+
+
+    }
+    public class Settings_Functions
+    {
+        public static Settings getSettings()
+        {
+            Settings st1 = new Settings();
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string[] SettingMain = File.ReadAllLines(Path.GetDirectoryName(path) + "\\start.properties");
+            st1.location_Workflows = getSettingINI(SettingMain, "workflows.directory");
+            st1.location_Install = getSettingINI(SettingMain, "install.directory");
+            st1.var_ENV_User = getSettingINI(SettingMain, "classpath.variable");
+            st1.var_ENV_User=st1.var_ENV_User.Replace("workflowslocaton", st1.location_Workflows);
+            st1.var_ENV_User=st1.var_ENV_User.Replace("installlocation", st1.location_Install);
+
+
+            return st1;
+        }
+        public static string getSettingINI(string[] str, string strName)
+        {
+            foreach (string str1 in str)
+            {
+                if (str1.Contains(strName))
+                {
+                    return str1.Split('=')[1];
+                }
+            }
+            return "none";
+        }
+    }
+}
